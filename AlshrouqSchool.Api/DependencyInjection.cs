0086d9bbf684e05c6362b251fac6891922d26b4d
Ayman604 <aymanelzoghby090@gmail.com>
2026-09-02 13:17:33 +0300

    Initial Clean Architecture setup

---
+++ AlshrouqSchool.Api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using AlshrouqSchool.Application;
+using AlshrouqSchool.Infrastructure;
+
+namespace AlshrouqSchool.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                    .AddInfrastructureDI();
+            return services;
+        }
+    }
+}
---
+++ AlshrouqSchool.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlshrouqSchool.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
